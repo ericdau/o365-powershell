ca0572ecf9b87d16fcbb39345a8cd6c3cd8279d9
ericdau <ericdau@gmail.com>
2019-05-14 21:08:59 -0700

    Initial upload

---
+++ connect.ps1
@@ -0,0 +1,5 @@
+#https://docs.microsoft.com/en-us/powershell/exchange/exchange-online/connect-to-exchange-online-powershell/connect-to-exchange-online-powershell?view=exchange-ps
+
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
